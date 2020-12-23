fa08def9b23a8d162cabf7d791d5700963091d0e
Yehuda Cohen <cohen72@gmail.com>
2020-12-23 17:55:33 +0200

    added tagging

---
+++ react-native-version.sh
@@ -0,0 +1,11 @@
+
+branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+
+echo "Running react-native-version on branch:${branch}"
+
+if [ $branch == 'test' ]
+then
+  react-native-version --never-amend
+else
+  react-native-version --never-amend --never-increment-build
+fi
