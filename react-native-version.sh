af79c0d06d73f1f3c2542787006df6449c153fb7
Yehuda Cohen <cohen72@gmail.com>
2020-12-27 15:16:54 +0200

    fix: shell script

--- react-native-version.sh
+++ react-native-version.sh
@@ -1,9 +1,9 @@
 
 branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
-echo "Running react-native-version on branch:${branch}"
+echo "Running react-native-version on branch: ${branch}"
 
-if [ $branch == 'master' ]
+if [ $branch == "master" ]
 then
   react-native-version --never-amend
 else
