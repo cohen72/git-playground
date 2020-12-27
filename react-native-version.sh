11dc95ca3bc4ffbfa221801efc00eb1034f3033f
Yehuda Cohen <cohen72@gmail.com>
2020-12-27 15:32:39 +0200

    fix: shell script

--- react-native-version.sh
+++ react-native-version.sh
@@ -5,7 +5,9 @@ echo "Running react-native-version on branch: ${branch}"
 
 if [ $branch == "master" ]
 then
+  echo "Running on master"
   react-native-version --never-amend
 else
+echo "Not on master, incrementing build"
   react-native-version --never-amend --never-increment-build
 fi
