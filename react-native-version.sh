c1cbd6e0525206e058b0294249dbc20272603328
Yehuda Cohen <cohen72@gmail.com>
2020-12-27 15:03:09 +0200

    fix: postversion

--- react-native-version.sh
+++ react-native-version.sh
@@ -3,7 +3,7 @@ branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 
 echo "Running react-native-version on branch:${branch}"
 
-if [ $branch == 'test' ]
+if [ $branch == 'master' ]
 then
   react-native-version --never-amend
 else
