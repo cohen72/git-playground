d3dabd46650f6407d8e35098c527b50f1ee627ca
Yehuda Cohen <cohen72@gmail.com>
2020-12-27 15:44:33 +0200

    fix: adding maintenance branches to release.yml

--- react-native-version.sh
+++ react-native-version.sh
@@ -6,8 +6,8 @@ echo "Running react-native-version on branch: ${branch}"
 if [ $branch == "master" ]
 then
   echo "Running on master"
-  react-native-version --never-amend
+  react-native-version --never-amend --never-increment-build
 else
 echo "Not on master, incrementing build"
-  react-native-version --never-amend --never-increment-build
+  react-native-version --never-amend
 fi
