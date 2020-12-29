066729ccd9541b8ac39008740e12a398871d992d
Yehuda Cohen <cohen72@gmail.com>
2020-12-29 17:44:07 +0200

    fix: test

---
+++ release.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+	branches: [
+		'+([0-9])?(.{+([0-9]),x}).x',
+		'master',
+		'next',
+		{
+			name: 'develop',
+			prerelease: 'qa',
+		},
+		{
+			name: 'beta',
+			prerelease: true,
+		},
+	],
+	npmPublish: true,
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		[
+			'@semantic-release/release-notes-generator',
+			{
+				writerOpts: {
+					// headerPartial: 'DUDE!',
+					transform: (commit, context) => {
+						context.title = 'YEUUUUUH';
+						return context;
+					},
+				},
+			},
+		],
+		'@semantic-release/npm',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['ios/ColuConsumerRn/Info.plist', 'ios/ColuConsumerRn.xcodeproj/project.pbxproj', 'docs/CHANGELOG.md', 'android/app/build.gradle', 'package.json'],
+				message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+			},
+		],
+		[
+			'@semantic-release/github',
+			{
+				successComment: false,
+				failComment: false,
+			},
+		],
+	],
+};
